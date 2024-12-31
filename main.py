bf392b91ba5cf66078437efd0f96bd205c0d9b9e
aishwinj04 <aishwin@gmail.com>
2024-12-30 22:49:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import cv2
 import os
 
+# rows and columns for collage
+columns = 3
+rows = 2
 
+# spacing inbetween each image and on the border 
+horizontal_margin = 40
+vertical_margin = 20
+
+# image path 
 images = os.listdir('images')
-print(images)
+
+# all images to be same size
+img1 = cv2.imread('images/img1.JPG')
+
+# .shape uses order of height then width 
+width = img1.shape[1] 
+height = img1.shape[0]
