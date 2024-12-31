17af9ba1b67af3982daf901c587ae989b639341a
aishwinj04 <aishwin@gmail.com>
2024-12-31 00:00:25 -0500

    Validate Images

--- main.py
+++ main.py
@@ -5,17 +5,25 @@ import numpy as np
 # rows and columns for collage
 columns = 3
 rows = 2
-
 # spacing inbetween each image and on the border 
 horizontal_margin = 40
 vertical_margin = 20
 
 # image path 
 images = os.listdir('images')
+# for macOS
+valid_images = []
+for filename in images:
+    if filename != '.DS_Store' :
+        valid_images.append(filename)
+print(valid_images)
+image_obj = [cv2.imread(f'images/{filename}') for filename in valid_images]
 
 # all images to be same size
 shape = cv2.imread('images/img1.JPG').shape
 
+
+# (3648, 5472, 3)
 # background using numpy
 
 # shape[0] * rows accounts for for the vertical size of only the images
@@ -30,6 +38,4 @@ background = np.zeros((shape[0]  * rows + horizontal_margin * (rows + 1),
                        shape[2]), 
                        np.uint8)
 
-background.fill(255)
-
-cv2.imwrite('grid.JPG', background)
+background.fill(255) # change to white
